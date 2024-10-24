175de4e9e54b71c8fae2ba458e0dbc861a0ab48e
Justin Hill <justin.hill@furnishedfinder.com>
2024-10-23 17:06:20 -0700

    bump to 1.1.3, update tsc settings

---
+++ src/sleep.js
@@ -0,0 +1,30 @@
+/**
+ * Pause execution for a number of milliseconds.
+ * @param millis Number of milliseconds to pause.
+ */
+export const sleep = async (millis) => new Promise((resolveMe) => setTimeout(resolveMe, millis));
+/**
+ * Pause execution for a number of milliseconds.
+ * @param millis Number of milliseconds to pause.
+ */
+sleep.millis = sleep;
+/**
+ * Pause execution for a number of seconds.
+ * @param seconds Number of seconds to pause.
+ */
+sleep.seconds = async (seconds) => sleep(seconds * 1000);
+/**
+ * Pause execution for a number of minutes.
+ * @param minutes Number of minutes to pause.
+ */
+sleep.minutes = async (minutes) => sleep(minutes * 60_000);
+/**
+ * Pause execution for a number of hours.
+ * @param hours Number of hours to pause.
+ */
+sleep.hours = async (hours) => sleep(hours * 3_600_000);
+/**
+ * Pause execution for a number of days.
+ * @param days Number of days to pause.
+ */
+sleep.days = async (days) => sleep(days * 86_400_000);
